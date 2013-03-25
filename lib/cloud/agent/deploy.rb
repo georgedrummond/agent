b7cb8f0e559c6b58dabf4720873a4f596f5698e7
George Drummond <georgedrummond@gmail.com>
2013-03-25 01:05:49 +0000

    ideas for dsl

--- lib/cloud/agent/deploy.rb
+++ lib/cloud/agent/deploy.rb
@@ -34,31 +34,45 @@ class Cloud::Agent::Deploy
     path = "/deploys/#{@deploy_token}"
     response = JSON.parse authenticated_request(:get, path)
     response.each { |k, v| self.class.send :define_method, k, proc {v} }
-    Cloud::Agent.logger.info(['deploy_response', path, response])
+    log.info ['deploy_response', path, response]
   rescue => e
-    Cloud::Agent::Error.notify('request_payload_invalid_response', e)
+    report_error 'request_payload_invalid_response', e
   end
 
   def download_deployment_archive!
     File.open(archive_download_path, 'wb') do |deployment_archive|
       open(archive_url) do |archive_download|
         deployment_archive.write archive_download.read
-        Cloud::Agent.logger.info(['download_deployment_archive', archive_url, archive_download_path])
+        log.info ['download_deployment_archive', archive_url, archive_download_path]
       end
     end
   rescue => e
-    File.delete(archive_download_path)
-    Cloud::Agent::Error.notify('download_deployment_archive_error', e) 
+    delete_file archive_download_path
+    report_error 'download_deployment_archive_error', e
   end
 
   def unpackage_deployment_archive!
     `mkdir #{latest_release_path} && tar zxf #{archive_download_path} -C #{latest_release_path}`
-    Cloud::Agent.logger.info(['extracted_deployment_archive', latest_release_path])
-    File.delete(archive_download_path)
-    Cloud::Agent.logger.info(['cleanup_deployment_archive', archive_download_path])
+    log.info ['extracted_deployment_archive', latest_release_path]
+    delete_file archive_download_path
+    log.info ['cleanup_deployment_archive', archive_download_path]
   end
 
   def symlink_current!
     File.symlink latest_release_path, current_release_path
+  end
+
+private
+
+  def log
+    Cloud::Agent.logger
+  end
+
+  def delete_file(path)
+    File.delete(path)
+  end
+
+  def report_error(name, error)
+    Cloud::Agent::Error.notify(name, error)
   end
 end
